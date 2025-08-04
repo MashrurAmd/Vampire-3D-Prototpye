c30e8abe3ef2e9035e52ad3e6d3e81c163e6888f
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-08-04 16:52:57 +0600

    player movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,34 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+
+public class PlayerMovement3D : MonoBehaviour
 {
-    Animator playerAnim;
+    public float moveSpeed = 20f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
 
-    private void Awake()
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    void Start()
     {
-        playerAnim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        // Check if player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
         {
-            playerAnim.SetBool("isRunning", true);
+            velocity.y = -2f; // Keeps player grounded
         }
+
+        // Get input
+        float x = Input.GetAxis("Horizontal"); // A/D
+        float z = Input.GetAxis("Vertical");   // W/S
+
+        // Move relative to the player's forward direction
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
