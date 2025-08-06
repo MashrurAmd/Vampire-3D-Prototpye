14538d3fb9f05d2f5b8c801bb652003358b6b832
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-08-06 11:33:06 +0600

    Add animation walk

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,40 +1,58 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement3D : MonoBehaviour
 {
     public GameObject player;
     public float moveSpeed = 5f;
     public float gravity = -9.81f;
+    public float jumpHeight = 2f; // Height of jump
 
     private CharacterController controller;
     private Vector3 velocity;
     private Animator animator;
 
-    private bool isRunning;
+    private bool isWalking;
 
     void Start()
     {
         controller = player.GetComponent<CharacterController>();
-        animator = player.GetComponent<Animator>(); // Make sure Animator is on the player
+        animator = player.GetComponentInChildren<Animator>(); 
     }
 
     void Update()
     {
-        // Get input
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // Determine running
-        isRunning = Input.GetKey(KeyCode.W) && z > 0;
-        animator.SetBool("isRunning", isRunning);
+        
+        isWalking = z != 0;
+        animator.SetBool("isWalking", isWalking);
 
-        // Move relative to the player's forward direction
+        
         Vector3 move = player.transform.right * x + player.transform.forward * z;
         controller.Move(move * moveSpeed * Time.deltaTime);
 
-        // Apply gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime); 
+        
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); // Apply jump velocity
+            animator.SetTrigger("isJumping"); 
+        }
 
+        // Apply gravity
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Keep grounded
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
+
+
+
+
+
+
